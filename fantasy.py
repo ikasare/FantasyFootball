f99c7b6a8858678443fa989b59f036bd92b40bdc
Isaac Asare <ikasar294@students.campbellsville.edu>
2021-07-06 20:50:17 +0000

    adding methods
    Co-authored-by: Oluwademilade Bolatimi <dpbolat19@earlham.edu>

--- fantasy.py
+++ fantasy.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import pandas as pd
+import os
+from sqlalchemy import create_engine
 
 #make user input team to look up its midfielders
 
@@ -28,25 +30,47 @@ def get_team_midfielders():
 
 def get_stats_for_game():
         url = "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/match/player"
+        col_names = ['Player Name', 'Touches', 'Passes Completed', 'Dribbles Won', 'Goals', 'Home Team', 'Away Team']
+        df = pd.DataFrame(columns=col_names)
         
-        team1 = 'Chelsea' #input('Please enter the home team: ')
-        team2 = 'Arsenal' #input('Please enter the away team: ')
-        player = 'Mason Mount' #input("Please enter the midfielder's name: ")
+        exit = ""
+        while exit != 'Q':
+            team1 = input('Please enter the home team: ')# 'Chelsea' 
+            team2 = input('Please enter the away team: ') #'Arsenal' 
+            player = input("Please enter the midfielder's name: ") #'Mason Mount' 
 
-        querystring = {'team1': team1, 'team2': team2, 'player': player}
+            querystring = {'team1': team1, 'team2': team2, 'player': player}
 
-        headers = {
-            'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
-            'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
-            }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        response = response.json()['stats']
-        print(player)
-        stats = {'touches': response.get('touches', 0),
-                 'Passes Completed': response.get('passesSuccessful', 0),
-                 'Dribbles Won': response.get('dribblesWon', 0),
-                 'Goals': response.get('goals', 0)}
-        print(stats)
+            headers = {
+                'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
+                'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
+                }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            response = response.json()['stats']
+            stats = {'Player Name': player,
+                     'Touches': response.get('touches', 0),
+                     'Passes Completed': response.get('passesSuccessful', 0),
+                     'Dribbles Won': response.get('dribblesWon', 0),
+                     'Goals': response.get('goals', 0)}
+            df.loc[len(df.index)] = [stats['Player Name'], stats['Touches'], stats['Passes Completed'], stats['Dribbles Won'], stats['Goals'], team1, team2]
+            exit  = input('Enter Q to exit, C to continue: ')
+        return df
+        
+        
+def createdb():
+        df = get_stats_for_game()
+        os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' + 'Premier_League' + ';"')
+        engine = create_engine('mysql://root:codio@localhost/Premier_League')
+        df.to_sql('Midfielders', con=engine, if_exists='replace', index=False)
 
+def savedb():
+    os.system("mysqldump -u root -pcodio Premier_League > epl.sql")
 
-get_stats_for_game()
+def loaddb():
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' +
+              'Premier_League' + ';"')
+    os.system("mysql -u root -pcodio Premier_League < epl.sql")
+
+createdb()
+savedb()
+                  
