ca1e4c6b5089ce820e4555823e00b9628b0de59e
Oluwademilade Bolatimi <demibotti2000@gmail.com>
2021-07-08 18:30:56 +0000

    fixed unittest
    Co-authored-by: Oluwademilade Bolatimi <dpbolat19@earlham.edu>

--- testing.py
+++ testing.py
@@ -6,16 +6,18 @@ from fantasy import get_team_midfielders, get_stats_for_game
 class TestFileName(unittest.TestCase):
         
     def test_get_team_midfielders(self):
+        test = get_team_midfielders()
         # Test is our function returns a list of midfielders 
-        self.assertTrue(type(get_team_midfielders()) is list)
+        self.assertTrue(type(test) is list)
         #Make sure that the list is not empty and has names and data of midfielder
-        self.assertTrue(len(get_team_midfielders()) > 0)
+        self.assertTrue(len(test) > 0)
 
     def test_get_stats_for_game(self):
+        test = get_stats_for_game()
         #Make sure the function returns a datframe with pandas
-        self.assertTrue(type(get_stats_for_game()) is pandas.core.frame.DataFrame)
+        self.assertTrue(type(test) is pandas.core.frame.DataFrame)
         #Make sure there is data in our dataFrame
-        self.assertFalse(get_stats_for_game().empty)
+        self.assertFalse(test.empty)
         
         
         
