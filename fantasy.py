0e7309c4d59d3441a74ba1f6dbba0a6ad65887df
Isaac Asare <ikasar294@students.campbellsville.edu>
2021-07-08 22:30:29 +0000

    styling

--- fantasy.py
+++ fantasy.py
@@ -8,105 +8,105 @@ from sqlalchemy import create_engine
 
 
 def get_team_midfielders():
-        test = []
-        teams = input("how many team rosters do you want to see: ")
-        if not teams.isdigit():
-            print('Please enter a number')
-            get_team_midfielders()
-        else:
-            while int(teams) > 0:
-                url = "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/players"
+    test = []
+    teams = input("how many team rosters do you want to see: ")
+    if not teams.isdigit():
+        print('Please enter a number')
+        get_team_midfielders()
+    else:
+        while int(teams) > 0:
+            url = "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/players"
 
-                querystring = {"team": input('Please enter a team whose midfielders you want to see: ')}
+            querystring = {"team": input('Please enter a team whose midfielders you want to see: ')}
 
-                headers = {
-                    'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
-                    'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
-                    }
+            headers = {
+                'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
+                'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
+                }
 
-                response = requests.request("GET", url, headers=headers, params=querystring)
-                if 'error' in response.text:
-                    print('Enter the correct team name')
-                    get_team_midfielders()
-                else:
-                    response = response.json()['players']
-                    test = response
-                    print(str(querystring['team']) + " midfelders: ")
-                    for player in response:
-                        if 'Midfield' in player['position']:
-                            print(player['playerName'])
-                    teams = int(teams)
-                    teams -= 1
-        return test
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            if 'error' in response.text:
+                print('Enter the correct team name')
+                get_team_midfielders()
+            else:
+                response = response.json()['players']
+                test = response
+                print(str(querystring['team']) + " midfelders: ")
+                for player in response:
+                    if 'Midfield' in player['position']:
+                        print(player['playerName'])
+                teams = int(teams)
+                teams -= 1
+    return test
 
 
 def get_stats_for_game():
-        url = "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/match/player"
-        col_names = ['Player Name', 'Touches', 'Passes Completed', 'Dribbles Won', 'Goals','Assists', 'Home Team', 'Away Team']
-        df = pd.DataFrame(columns=col_names)
+    url = "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/match/player"
+    col_names = ['Player Name', 'Touches', 'Passes Completed', 'Dribbles Won', 'Goals', 'Assists', 'Home Team', 'Away Team']
+    df = pd.DataFrame(columns=col_names)
 
-        exit = ""
-        match = ""
-        while exit.upper() != 'Q':
-            amount = input("How many midfileders would you like to choose from this match: ")
-            if not amount.isdigit():
-                print('Please enter the number of midfielders you would like to analyze from a match: ')
+    exit = ""
+    match = ""
+    while exit.upper() != 'Q':
+        amount = input("How many midfileders would you like to choose from this match: ")
+        if not amount.isdigit():
+            print('Please enter the number of midfielders you would like to analyze from a match: ')
+            get_stats_for_game()
+        team1 = 'Chelsea'  # input('Please enter the home team: ') # 'Chelsea'
+        team2 = 'Arsenal'  # input('Please enter the away team: ') # 'Arsenal'
+        while int(amount) > 0:
+            player = 'Mason Mount'  # input("Please enter the midfielder's name: ") #'Mason Mount' 
+            querystring = {'team1': team1, 'team2': team2, 'player': player}
+            headers = {
+                'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
+                'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
+                }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            if 'error' in response.text:
+                print('Enter correct player name or team name')
                 get_stats_for_game()
-            team1 = 'Chelsea'  # input('Please enter the home team: ') # 'Chelsea'
-            team2 = 'Arsenal'  # input('Please enter the away team: ') # 'Arsenal'
-            while int(amount) > 0:       
-                player = 'Mason Mount' #input("Please enter the midfielder's name: ") #'Mason Mount' 
-                querystring = {'team1': team1, 'team2': team2, 'player': player}
-                headers = {
-                    'x-rapidapi-key': "d1d4b18bb8mshb6f0f40d2cc89ddp13206bjsnaa6185b9070d",
-                    'x-rapidapi-host': "heisenbug-premier-league-live-scores-v1.p.rapidapi.com"
-                    }
-                response = requests.request("GET", url, headers=headers, params=querystring)
-                if 'error' in response.text:
-                    print('Enter correct player name or team name')
-                    get_stats_for_game()
-                else:
-                    response = response.json()['stats']
-                    stats = {'Player Name': player,
-                             'Touches': response.get('touches', 0),
-                             'Passes Completed': response.get('passesSuccessful', 0),
-                             'Dribbles Won': response.get('dribblesWon', 0),
-                             'Goals': response.get('goals', 0),
-                             'Assists': response.get('assists',0)}
-                    df.loc[len(df.index)] = [stats['Player Name'], stats['Touches'], stats['Passes Completed'],
-                                             stats['Dribbles Won'], stats['Goals'], stats['Assists'], team1, team2]
-                    amount= int(amount)
-                    amount-= 1
-            exit  = input('Enter Q to exit, or C to choose different match: ')        
-        print(df)
-        return df
-        
-        
+            else:
+                response = response.json()['stats']
+                stats = {'Player Name': player,
+                         'Touches': response.get('touches', 0),
+                         'Passes Completed': response.get('passesSuccessful', 0),
+                         'Dribbles Won': response.get('dribblesWon', 0),
+                         'Goals': response.get('goals', 0),
+                         'Assists': response.get('assists',0)}
+                df.loc[len(df.index)] = [stats['Player Name'], stats['Touches'], stats['Passes Completed'],
+                                         stats['Dribbles Won'], stats['Goals'], stats['Assists'], team1, team2]
+                amount = int(amount)
+                amount -= 1
+        exit = input('Enter Q to exit, or C to choose different match: ')        
+    print(df)
+    return df
+
+
 def createdb():
-        df = get_stats_for_game()
-        os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' + 'Premier_League' + ';"')
-        engine = create_engine('mysql://root:codio@localhost/Premier_League')
-        df.to_sql('Midfielders', con=engine, if_exists='replace', index=False)
+    df = get_stats_for_game()
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' + 'Premier_League' + ';"')
+    engine = create_engine('mysql://root:codio@localhost/Premier_League')
+    df.to_sql('Midfielders', con=engine, if_exists='replace', index=False)
         
 
 def savedb():
-        os.system("mysqldump -u root -pcodio Premier_League > epl.sql")
+    os.system("mysqldump -u root -pcodio Premier_League > epl.sql")
 
 
 def loaddb():
-        os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' +
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' +
               'Premier_League' + ';"')
-        os.system("mysql -u root -pcodio Premier_League < epl.sql")
+    os.system("mysql -u root -pcodio Premier_League < epl.sql")
     
 
 def loadDataset(update=False):
-        engine = create_engine('mysql://root:codio@localhost/Premier_League')
-        loaddb()
-        df = pd.read_sql_table('Midfielders', con=engine)
-        return df
+    engine = create_engine('mysql://root:codio@localhost/Premier_League')
+    loaddb()
+    df = pd.read_sql_table('Midfielders', con=engine)
+    return df
   
 
 if __name__ == '__main__': 
-        get_team_midfielders()
-        createdb()
-        savedb()
+    get_team_midfielders()
+    createdb()
+    savedb()
--- testing.py
+++ testing.py
@@ -3,25 +3,23 @@ import pandas
 
 from fantasy import get_team_midfielders, get_stats_for_game
 
+
 class TestFileName(unittest.TestCase):
         
     def test_get_team_midfielders(self):
         test = get_team_midfielders()
         # Test is our function returns a list of midfielders 
         self.assertTrue(type(test) is list)
-        #Make sure that the list is not empty and has names and data of midfielder
+        # Make sure that the list is not empty and has names and data of midfielder
         self.assertTrue(len(test) > 0)
 
     def test_get_stats_for_game(self):
         test = get_stats_for_game()
-        #Make sure the function returns a datframe with pandas
+        # Make sure the function returns a datframe with pandas
         self.assertTrue(type(test) is pandas.core.frame.DataFrame)
-        #Make sure there is data in our dataFrame
+        # Make sure there is data in our dataFrame
         self.assertFalse(test.empty)
-        
-        
         
 
 if __name__ == '__main__':
     unittest.main()
-
